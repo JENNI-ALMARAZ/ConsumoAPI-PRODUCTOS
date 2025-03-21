5fcc263790901b45d41b765d6b192ed4f84c09d5
Jennifer <almarazjennifer44@gmail.com>
2025-03-21 09:33:03 -0600

    Primer commit

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import './App.css'
+import ProductTable from "../src/Components/ProductTable";
+
+function App() {
+  return (
+    <div className="min-h-screen bg-gray-100 p-6">
+      <ProductTable />
+    </div>
+  );
+}
+
+export default App;
