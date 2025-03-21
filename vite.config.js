4914fff9d416c1d471257186d982a2c6627a1392
JENNI-ALMARAZ <91512122+JENNI-ALMARAZ@users.noreply.github.com>
2025-03-21 10:04:32 -0600

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -2,7 +2,8 @@ import { defineConfig } from 'vite';
 
 export default defineConfig({
   server: {
-    port: process.env.PORT || 3000,  // Usa el puerto de Render o 3000 si no está definido
     host: '0.0.0.0',  // Asegúrate de que Vite escuche en todas las interfaces de red
+    port: process.env.PORT || 3000,  // Usa el puerto proporcionado por Render o 3000 si no está definido
+    allowedHosts: ['consumoapi-productos.onrender.com'],  // Permite el dominio de Render
   },
 });
